2e261a28fcf78db87ace44e9ccf214a8fd5b776a
kiwi0fruit <peter.zagubisalo@gmail.com>
2019-02-08 14:32:32 +0700

    commit

---
+++ pypi/py-pandoc/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+__version__ = '2.6'
+
+setup(
+    name='py-pandoc',
+    version=__version__,
+
+    description='Pandoc in pip and conda',
+    url='https://github.com/kiwi0fruit/py-pandoc',
+    author='kiwi0fruit',
+    author_email='peter.zagubisalo@gmail.com',
+    license='GPLv2+',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
