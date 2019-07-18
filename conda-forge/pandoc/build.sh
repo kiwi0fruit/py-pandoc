5e7b0cfce6b24487d06944473593d116bebfb7f2
kiwi0fruit <peter.zagubisalo@gmail.com>
2019-07-18 15:34:02 +0700

    backup 2.7.3 conda-forge pandoc

--- conda-forge/pandoc/build.sh
+++ conda-forge/pandoc/build.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
+mkdir -p ${PREFIX}/bin
+
 if [ $(uname) == Linux ]; then
-    # conda-build tries to be smarted than you an unpackas debs now :-/
-    # ar vx pandoc.deb
-    tar --extract --xz --verbose --file=data.tar.xz
-    mkdir -p $PREFIX/bin
-    mv usr/bin/* $PREFIX/bin
+    mv bin/* ${PREFIX}/bin
 fi
 
 
 if [ $(uname) == Darwin ]; then
     pkgutil --expand pandoc.pkg pandoc
     cpio -i -I pandoc/pandoc.pkg/Payload
-    mkdir -p $PREFIX/bin
-    cp usr/local/bin/* $PREFIX/bin/
+    cp usr/local/bin/* ${PREFIX}/bin/
 fi
